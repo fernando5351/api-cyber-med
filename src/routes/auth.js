ed0ad2d98e615a31f49f68278f8c5da3eac6f2e0
fernando5351 <isaacfernando5351@gmail.com>
2022-08-25 19:58:30 -0600

    arreglando ruta

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { register, login } = require('../controllers/authController');
 
-router.post('/login', register );
+router.post('/register', register );
 router.post('/login', login );
 
 module.exports = router;
