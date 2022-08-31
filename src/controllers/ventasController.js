ac15f87464e39ab3564be97198128ba3e56f0d7b
fernando5351 <isaacfernando5351@gmail.com>
2022-08-31 15:35:37 -0600

    log out finished

--- src/controllers/ventasController.js
+++ src/controllers/ventasController.js
@@ -1,19 +1,21 @@
 const { factory } = require("../factory/quey_factory");
 const qrcode = require("qrcode");
-const fs = require("fs");
 
 const generator = async (req, res) => {
+  const { id } = req.params
   const query =
-    "SELECT productos.nombre, datos_pedido.cantidad FROM productos, datos_pedido WHERE datos_pedido.id_producto LIKE productos.id;";
+    `SELECT productos.nombre, datos_pedido.cantidad FROM productos, datos_pedido WHERE id LIKE ${id} && datos_pedido.id_producto LIKE productos.id;`;
 
-  let sql = await factory(query);
+  let sql = await factory(query)
 
-  const QR = await qrcode.toDataURL(sql);
+  let stringData = JSON.stringify(sql);
+
+  const QR = await qrcode.toDataURL(stringData);
 
   res.json(QR);
 
-  console.log(QR)
-  console.log(sql)
+  console.log(QR);
+  console.log(sql);
 };
 
 module.exports = {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const { register, login } = require('../controllers/authController');
+const { register, login, log_out } = require('../controllers/authController');
 
 router.post('/register', register );
 router.post('/login', login );
+router.get('/log_out', log_out);
 
 module.exports = router;
