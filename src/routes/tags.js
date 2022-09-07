310f2ace0c06e39748fedca819a7beca39c04bcc
fernando5351 <isaacfernando5351@gmail.com>
2022-09-06 20:15:57 -0600

    agregando obtencion de productos por tipo de consumo

--- src/routes/tags.js
+++ src/routes/tags.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTags } = require('../controllers/tagsController')
+const { getTags, filter } = require('../controllers/tagsController')
 
 router.get('/view/tags-products', getTags);
+router.get('/filter/:id', filter)
 
 module.exports = router
