4c4c9b337f537dab794dd3d384369e6a26c0a8f9
fernando5351 <isaacfernando5351@gmail.com>
2022-09-08 07:51:08 -0600

    arreglando bug de compra de productos

--- src/controllers/ventasController.js
+++ src/controllers/ventasController.js
@@ -4,7 +4,11 @@ const qrcode = require("qrcode");
 const generator = async (req, res) => {
   const { id } = req.params
   const query =
-    `SELECT productos.nombre, datos_pedido.cantidad FROM productos, datos_pedido WHERE id LIKE ${id} && datos_pedido.id_producto LIKE productos.id;`;
+    `SELECT productos.nombre, 
+        datos_pedido.cantidad 
+    FROM productos, datos_pedido 
+    WHERE datos_pedido.id LIKE ${id} && 
+        datos_pedido.id_producto LIKE productos.id;`;
 
   let sql = await factory(query)
 
