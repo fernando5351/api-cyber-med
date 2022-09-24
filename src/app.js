640c964dc112da8098c176398742c945b1b49eda
fernando5351 <isaacfernando5351@gmail.com>
2022-09-24 17:57:16 -0600

    cors agregando origin

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const app = express();
 
 //settings
 app.set("port", port);
+
 var corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
