d3b8a4ea4b842c62efabc177c483bb20a59765aa
fernando5351 <isaacfernando5351@gmail.com>
2022-10-20 11:14:53 -0600

    agregando img al endpoint carrito

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -19,7 +19,7 @@ async function getCarShop(req, res) {
   console.log(validation);
 
   if (sql[0].estado === 1) {
-    const query = `SELECT productos.nombre, productos.precios, datos_pedido.id_producto, datos_pedido.id_cliente, clientes.nombres, datos_pedido.cantidad, datos_pedido.estado FROM clientes, datos_pedido, productos WHERE datos_pedido.id_cliente = ${id} && productos.id  = datos_pedido.id_producto && datos_pedido.id_cliente = clientes.id;`;
+    const query = `SELECT productos.nombre, productos.img_url, productos.precios, datos_pedido.id_producto, datos_pedido.id_cliente, clientes.nombres, datos_pedido.cantidad, datos_pedido.estado FROM clientes, datos_pedido, productos WHERE datos_pedido.id_cliente = ${id} && productos.id  = datos_pedido.id_producto && datos_pedido.id_cliente = clientes.id;`;
     const response = await factory(query);
     console.log(query);
 
