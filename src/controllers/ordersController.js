9a95a63e2ba78f4a149476b63b216f6339aff30c
fernando5351 <isaacfernando5351@gmail.com>
2022-10-21 19:18:09 -0600

    arreglando problema con carrito

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -1,9 +1,9 @@
 const { factory, connection } = require("../factory/quey_factory");
 
 async function carshop(req, res) {
-  const { id_producto, id_cliente, cantidad, estado } = req.body;
+  const { id_producto, id_cliente, cantidad } = req.body;
   console.log(req.body);
-  let query = `INSERT INTO datos_pedido(id_producto, id_cliente, cantidad, estado) VALUES (${id_producto}, ${id_cliente}, ${cantidad}, (1))`;
+  let query = `INSERT INTO datos_pedido(id_producto, id_cliente, cantidad, estado) VALUES (${id_producto}, ${id_cliente}, ${cantidad}, "1")`;
   const response = await factory(query);
 
   res.json(response);
@@ -13,28 +13,22 @@ async function carshop(req, res) {
 async function getCarShop(req, res) {
   const { id } = req.params;
 
-  let validation = `SELECT estado FROM datos_pedido WHERE id_cliente = ${id}`;
-
-  connection.query(validation, async (err, results) => {
-    if (results.length >= 1 && results.estado === 1) {
-      const query = `SELECT datos_pedido.id, productos.nombre, productos.img_url, productos.precios, datos_pedido.id_producto, datos_pedido.id_cliente, clientes.nombres, datos_pedido.cantidad, datos_pedido.estado FROM clientes, datos_pedido, productos WHERE datos_pedido.id_cliente = ${id} && productos.id  = datos_pedido.id_producto && datos_pedido.id_cliente = clientes.id;`;
-      const response = await factory(query);
-      console.log(query);
-
-      console.log(id);
-      res.json(response);
+  const query = `SELECT datos_pedido.id, productos.nombre, productos.img_url, productos.precios, datos_pedido.id_producto, datos_pedido.id_cliente, clientes.nombres, datos_pedido.cantidad, datos_pedido.estado FROM clientes, datos_pedido, productos WHERE datos_pedido.id_cliente = ${id} && productos.id  = datos_pedido.id_producto && datos_pedido.id_cliente = clientes.id && datos_pedido.estado = 1;`;
+  connection.query(query, async (err, results) => {
+    if (results.length >= 1) {
+      res.json(results);
     } else {
       res.json({ message: "no hay ningun producto en cuestion" });
     }
   });
 }
 
-async function delCarShop ( req, res ) {
-  const { id } = req.params
-  let query = `DELETE FROM datos_pedido WHERE id = ${id}`
+async function delCarShop(req, res) {
+  const { id } = req.params;
+  let query = `DELETE FROM datos_pedido WHERE id = ${id}`;
   query = await factory(query);
 
-  res.json(query)
+  res.json(query);
 }
 
 module.exports = {
